f235f7087ab44a3be17a68f24d5eab0cf02f532d
Kazi Ashik Al Islam <54812115+ashik-info@users.noreply.github.com>
2026-09-05 23:04:05 +0600

    Add citruszest configuration for Neovim

---
+++ citruszest.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "zootedb0t/citruszest.nvim",
+    lazy = false,
+    priority = 1000,
+
+    opts = {
+      option = {
+        transparent = false,
+        bold = false,
+        italic = true,
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "citruszest",
+    },
+  },
+}
